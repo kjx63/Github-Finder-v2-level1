8e41065c0005e9a87ba5e71bbcbc0d2cc606d071
kjx63 <ueyama.kenji.63@gmail.com>
2020-09-04 11:57:13 +0900

    Simplify by removing constructor method (11)

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 
 class UserItem extends Component {
   // add state to classbased component
-  constructor() {
-    super();
-    // create the classbased component
-    this.state = {
-      id: 'id',
-      login: 'mojombo',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo',
-    };
-  }
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo',
+  };
+
   render() {
     return (
       <div className='card text-center'>
